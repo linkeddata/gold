f259efda635c4f3b54590f3b0b8dbbebdb5eef1a
joe <presbrey+git@gmail.com>
2014-01-31 17:16:56 -0500

    add server tests w/ Turtle

---
+++ server_test.go
@@ -0,0 +1,51 @@
+package gold
+
+import (
+	"github.com/drewolson/testflight"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+var (
+	handler = Handler{}
+)
+
+func TestTurtlePOST(t *testing.T) {
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		response := r.Post("/abc", "text/turtle", "<a> <b> <c1> .")
+		assert.Equal(t, 200, response.StatusCode)
+	})
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		response := r.Post("/abc", "text/turtle", "<a> <b> <c2> .")
+		assert.Equal(t, 200, response.StatusCode)
+	})
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		request, _ := http.NewRequest("GET", "/abc", nil)
+		request.Header.Add("Accept", "text/turtle")
+		response := r.Do(request)
+		assert.Equal(t, 200, response.StatusCode)
+		assert.Equal(t, response.Body, "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\n<a>\n    <b> <c1>, <c2> .\n\n")
+	})
+}
+
+func TestTurtlePUT(t *testing.T) {
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		response := r.Put("/abc", "text/turtle", "<d> <e> <f> .")
+		assert.Equal(t, 200, response.StatusCode)
+	})
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		request, _ := http.NewRequest("GET", "/abc", nil)
+		request.Header.Add("Accept", "text/turtle")
+		response := r.Do(request)
+		assert.Equal(t, 200, response.StatusCode)
+		assert.Equal(t, response.Body, "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\n<d>\n    <e> <f> .\n\n")
+	})
+}
+
+func TestDELETE(t *testing.T) {
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		response := r.Delete("/abc", "", "")
+		assert.Equal(t, 200, response.StatusCode)
+	})
+}
