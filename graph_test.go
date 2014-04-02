0b7a6147233fe8d7405531a198ce4f4a2c16f17f
deiu <andrei@fcns.eu>
2014-04-02 14:31:26 -0400

    Added tests for One() and All()

--- graph_test.go
+++ graph_test.go
@@ -1,6 +1,7 @@
 package gold
 
 import (
+	rdf "github.com/kierdavis/argo"
 	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
@@ -27,4 +28,39 @@ func TestPatch(t *testing.T) {
 	buf, err = graph.Serialize("text/turtle")
 	assert.Nil(t, err)
 	assert.Equal(t, buf, "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\n<a>\n    <b> <c2> ;\n    <b2> <c2> .\n\n")
+}
+
+func TestOne(t *testing.T) {
+	g := NewGraph("http://test/")
+
+	g.AddTriple(rdf.NewResource("a"), rdf.NewResource("b"), rdf.NewResource("c"))
+	g.AddTriple(rdf.NewResource("a"), rdf.NewResource("b"), rdf.NewResource("d"))
+	g.AddTriple(rdf.NewResource("g"), rdf.NewResource("b2"), rdf.NewResource("e"))
+
+	assert.Equal(t, g.One(rdf.NewResource("a"), nil, nil).String(), "<a> <b> <c> .")
+	assert.Equal(t, g.One(rdf.NewResource("a"), rdf.NewResource("b"), nil).String(), "<a> <b> <c> .")
+	assert.Equal(t, g.One(rdf.NewResource("a"), rdf.NewResource("b"), rdf.NewResource("d")).String(), "<a> <b> <d> .")
+
+	assert.Equal(t, g.One(nil, rdf.NewResource("b"), rdf.NewResource("d")).String(), "<a> <b> <d> .")
+	assert.Equal(t, g.One(nil, rdf.NewResource("b2"), nil).String(), "<g> <b2> <e> .")
+	assert.Equal(t, g.One(nil, nil, rdf.NewResource("e")).String(), "<g> <b2> <e> .")
+
+	assert.Nil(t, g.One(rdf.NewResource("x"), nil, nil))
+	assert.Nil(t, g.One(nil, rdf.NewResource("x"), nil))
+	assert.Nil(t, g.One(nil, nil, rdf.NewResource("x")))
+}
+
+func TestAll(t *testing.T) {
+	g := NewGraph("http://test/")
+
+	g.AddTriple(rdf.NewResource("a"), rdf.NewResource("b"), rdf.NewResource("c"))
+	g.AddTriple(rdf.NewResource("a"), rdf.NewResource("b"), rdf.NewResource("d"))
+	g.AddTriple(rdf.NewResource("g"), rdf.NewResource("b2"), rdf.NewResource("e"))
+	g.AddTriple(rdf.NewResource("g"), rdf.NewResource("b2"), rdf.NewResource("c"))
+
+	assert.Equal(t, len(g.All(nil, nil, nil)), 0)
+	assert.Equal(t, len(g.All(rdf.NewResource("a"), nil, nil)), 2)
+	assert.Equal(t, len(g.All(nil, rdf.NewResource("b"), nil)), 2)
+	assert.Equal(t, len(g.All(nil, nil, rdf.NewResource("d"))), 1)
+	assert.Equal(t, len(g.All(nil, nil, rdf.NewResource("c"))), 2)
 }
