dcb3b8ad75d217753313e11b9cb5f89967da8dfe
deiu <andrei@fcns.eu>
2014-05-29 10:35:33 -0400

    Added a new test for defrag().

--- rdf_test.go
+++ rdf_test.go
@@ -18,3 +18,8 @@ func TestRDFDebrack(t *testing.T) {
 	assert.Equal(t, "<test", debrack("<test"))
 	assert.Equal(t, "test>", debrack("test>"))
 }
+
+func TestDefrag(t *testing.T) {
+	assert.Equal(t, "test", defrag("test"))
+	assert.Equal(t, "test", defrag("test#me"))
+}
