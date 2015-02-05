35885d5ac061c39d4ee6404627a1e6320036ff8b
deiu <andrei@fcns.eu>
2015-02-05 15:20:44 -0500

    Added cert generator for SPKAC requests

---
+++ spkac.go
@@ -0,0 +1,269 @@
+package gold
+
+import (
+	"bytes"
+	"crypto/dsa"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"math/big"
+)
+
+type pkacInfo struct {
+	Raw       asn1.RawContent
+	PublicKey publicKeyInfo
+	Challenge string
+}
+
+type spkacInfo struct {
+	Raw       asn1.RawContent
+	Pkac      pkacInfo
+	Algorithm pkix.AlgorithmIdentifier
+	Signature asn1.BitString
+}
+
+type rsaPublicKey struct {
+	N *big.Int
+	E int
+}
+
+type publicKeyInfo struct {
+	Raw       asn1.RawContent
+	Algorithm pkix.AlgorithmIdentifier
+	PublicKey asn1.BitString
+}
+
+type dsaAlgorithmParameters struct {
+	P, Q, G *big.Int
+}
+
+var (
+	oidNamedCurveP224 = asn1.ObjectIdentifier{1, 3, 132, 0, 33}
+	oidNamedCurveP256 = asn1.ObjectIdentifier{1, 2, 840, 10045, 3, 1, 7}
+	oidNamedCurveP384 = asn1.ObjectIdentifier{1, 3, 132, 0, 34}
+	oidNamedCurveP521 = asn1.ObjectIdentifier{1, 3, 132, 0, 35}
+
+	oidPublicKeyRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
+	oidPublicKeyDSA   = asn1.ObjectIdentifier{1, 2, 840, 10040, 4, 1}
+	oidPublicKeyECDSA = asn1.ObjectIdentifier{1, 2, 840, 10045, 2, 1}
+)
+
+func namedCurveFromOID(oid asn1.ObjectIdentifier) elliptic.Curve {
+	switch {
+	case oid.Equal(oidNamedCurveP224):
+		return elliptic.P224()
+	case oid.Equal(oidNamedCurveP256):
+		return elliptic.P256()
+	case oid.Equal(oidNamedCurveP384):
+		return elliptic.P384()
+	case oid.Equal(oidNamedCurveP521):
+		return elliptic.P521()
+	}
+	return nil
+}
+
+func parsePublicKey(algo x509.PublicKeyAlgorithm, keyData *publicKeyInfo) (interface{}, error) {
+	asn1Data := keyData.PublicKey.RightAlign()
+	switch algo {
+	case x509.RSA:
+		p := new(rsaPublicKey)
+		_, err := asn1.Unmarshal(asn1Data, p)
+		if err != nil {
+			return nil, err
+		}
+
+		if p.N.Sign() <= 0 {
+			return nil, errors.New("x509: RSA modulus is not a positive number")
+		}
+		if p.E <= 0 {
+			return nil, errors.New("x509: RSA public exponent is not a positive number")
+		}
+
+		pub := &rsa.PublicKey{
+			E: p.E,
+			N: p.N,
+		}
+		return pub, nil
+	case x509.DSA:
+		var p *big.Int
+		_, err := asn1.Unmarshal(asn1Data, &p)
+		if err != nil {
+			return nil, err
+		}
+		paramsData := keyData.Algorithm.Parameters.FullBytes
+		params := new(dsaAlgorithmParameters)
+		_, err = asn1.Unmarshal(paramsData, params)
+		if err != nil {
+			return nil, err
+		}
+		if p.Sign() <= 0 || params.P.Sign() <= 0 || params.Q.Sign() <= 0 || params.G.Sign() <= 0 {
+			return nil, errors.New("x509: zero or negative DSA parameter")
+		}
+		pub := &dsa.PublicKey{
+			Parameters: dsa.Parameters{
+				P: params.P,
+				Q: params.Q,
+				G: params.G,
+			},
+			Y: p,
+		}
+		return pub, nil
+	case x509.ECDSA:
+		paramsData := keyData.Algorithm.Parameters.FullBytes
+		namedCurveOID := new(asn1.ObjectIdentifier)
+		_, err := asn1.Unmarshal(paramsData, namedCurveOID)
+		if err != nil {
+			return nil, err
+		}
+		namedCurve := namedCurveFromOID(*namedCurveOID)
+		if namedCurve == nil {
+			return nil, errors.New("x509: unsupported elliptic curve")
+		}
+		x, y := elliptic.Unmarshal(namedCurve, asn1Data)
+		if x == nil {
+			return nil, errors.New("x509: failed to unmarshal elliptic curve point")
+		}
+		pub := &ecdsa.PublicKey{
+			Curve: namedCurve,
+			X:     x,
+			Y:     y,
+		}
+		return pub, nil
+	default:
+		return nil, nil
+	}
+}
+
+func getPublicKeyAlgorithmFromOID(oid asn1.ObjectIdentifier) x509.PublicKeyAlgorithm {
+	switch {
+	case oid.Equal(oidPublicKeyRSA):
+		return x509.RSA
+	case oid.Equal(oidPublicKeyDSA):
+		return x509.DSA
+	case oid.Equal(oidPublicKeyECDSA):
+		return x509.ECDSA
+	}
+	return x509.UnknownPublicKeyAlgorithm
+}
+
+// ParseSPKAC returns the public key from a KEYGEN's SPKAC request
+func ParseSPKAC(derBytes []byte) (pub interface{}, err error) {
+
+	var info spkacInfo
+	if _, err = asn1.Unmarshal(derBytes, &info); err != nil {
+		return
+	}
+
+	algo := getPublicKeyAlgorithmFromOID(info.Pkac.PublicKey.Algorithm.Algorithm)
+	if algo == x509.UnknownPublicKeyAlgorithm {
+		return nil, errors.New("x509: unknown public key algorithm")
+	}
+
+	pub, err = parsePublicKey(algo, &info.Pkac.PublicKey)
+	if err != nil {
+		return
+	}
+
+	return
+
+}
+
+// NewSPKACx509 creates a new x509 self-signed cert based on the SPKAC value
+func NewSPKACx509(uri string, name string, spkacBase64 string) ([]byte, error) {
+	spkacDerBytes, _ := base64.StdEncoding.DecodeString(spkacBase64)
+	public, err := ParseSPKAC(spkacDerBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	priv, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		return nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: new(big.Int).SetInt64(0),
+		Subject: pkix.Name{
+			CommonName:   name,
+			Organization: []string{"WebID"},
+			// Country:      []string{"US"},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		BasicConstraintsValid: true,
+		// KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		// ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+	}
+	rawValues := []asn1.RawValue{
+		{Class: 0, Tag: 16, IsCompound: true, Bytes: []byte(uri)},
+	}
+	values, err := asn1.Marshal(rawValues)
+	if err != nil {
+		return nil, err
+	}
+
+	template.ExtraExtensions = []pkix.Extension{{Id: subjectAltName, Value: values}}
+
+	certDerBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, public, priv)
+
+	return certDerBytes, nil
+}
+
+// NewRSAcert creates a new RSA x509 self-signed certificate
+func NewRSAcert(uri string, name string, priv *rsa.PrivateKey) (*tls.Certificate, error) {
+	template := x509.Certificate{
+		SerialNumber: new(big.Int).SetInt64(0),
+		Subject: pkix.Name{
+			CommonName:   name,
+			Organization: []string{"WebID"},
+			// Country:      []string{"US"},
+		},
+		// MaxPathLen: -1,
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		BasicConstraintsValid: true,
+		// KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		// ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+	}
+	rawValues := []asn1.RawValue{
+		{Class: 0, Tag: 16, IsCompound: true, Bytes: []byte(uri)},
+	}
+	values, err := asn1.Marshal(rawValues)
+	if err != nil {
+		return nil, err
+	}
+	template.ExtraExtensions = []pkix.Extension{{Id: subjectAltName, Value: values}}
+
+	keyPEM := bytes.NewBuffer(nil)
+	err = pem.Encode(keyPEM, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	if err != nil {
+		return nil, err
+	}
+
+	certPEM := bytes.NewBuffer(nil)
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return nil, err
+	}
+	err = pem.Encode(certPEM, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	if err != nil {
+		return nil, err
+	}
+
+	cert, err := tls.X509KeyPair(certPEM.Bytes(), keyPEM.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	return &cert, nil
+}
