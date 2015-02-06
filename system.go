ee4392658186166f9470dac0e9e5aeebb41b6498
deiu <andrei@fcns.eu>
2015-02-06 18:19:33 -0500

    Working profile cert generator (except for Chrome)

--- system.go
+++ system.go
@@ -1,10 +1,14 @@
 package gold
 
 import (
+	"crypto/rsa"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	_path "path"
 	"strings"
 )
 
@@ -25,22 +29,99 @@ type statusResponse struct {
 	Response accountResponse `json:"response"`
 }
 
-func HandleSystem(w http.ResponseWriter, req *httpRequest, vhosts bool, root string, resource ldpath) (int, string) {
-	if strings.Contains(resource.Path, "accountStatus") {
+// HandleSystem is a router for system specific APIs
+func HandleSystem(w http.ResponseWriter, req *httpRequest, s *Server) (int, string) {
+	if strings.Contains(req.BaseURI(), "accountStatus") {
 		// unsupported yet when server is running on one host
-		if vhosts == true {
-			status, payload := accountStatus(w, req, root, resource)
+		if s.vhosts == true {
+			status, payload := accountStatus(w, req, s)
 			return status, payload
 		}
+	} else if strings.Contains(req.BaseURI(), "spkac") {
+		status, payload := spkacNew(w, req, s)
+		return status, payload
 	}
 	return 200, ""
 }
 
-func spkac() {
+func spkacNew(w http.ResponseWriter, req *httpRequest, s *Server) (int, string) {
+	resource, _ := s.pathInfo(req.BaseURI())
 
+	username := strings.ToLower(req.FormValue("username"))
+	webidPath := s.root + username + "." + resource.Root + "/profile/"
+	webidFile := webidPath + "card"
+	port := ""
+	if ServerPort != ":443" || ServerPort != ":80" {
+		port = ServerPort
+	}
+	webidURI := "https://" + username + "." + resource.Root + port + "/profile/card#me"
+
+	spkac := req.FormValue("SPKAC")
+
+	account := webidAccount{
+		URI:   webidURI,
+		Name:  req.FormValue("name"),
+		Email: req.FormValue("email"),
+		Img:   req.FormValue("img"),
+	}
+
+	// create a new x509 cert based on the public key
+	certName := account.Name + " [on " + username + "." + resource.Root + "]"
+	newSpkac, err := NewSPKACx509(webidURI, certName, spkac)
+	if err != nil {
+		DebugLog("System", "SPKAC NewSPKACx509 error: "+err.Error())
+		return 500, err.Error()
+	}
+
+	// get public key from spkac
+	pubKey, err := ParseSPKAC(spkac)
+	if err != nil {
+		DebugLog("System", "SPKAC parse error: "+err.Error())
+		return 500, err.Error()
+	}
+	rsaPub := pubKey.(*rsa.PublicKey)
+	account.Modulus = fmt.Sprintf("%x", rsaPub.N)
+	account.Exponent = fmt.Sprintf("%d", rsaPub.E)
+
+	// Get WebID profile graph for this account
+	g := NewWebIDProfile(account)
+
+	// create account space
+	err = os.MkdirAll(_path.Dir(webidPath), 0755)
+	if err != nil {
+		DebugLog("Server", "SPKAC MkdirAll error: "+err.Error())
+		return 500, err.Error()
+	}
+
+	// open WebID profile file
+	f, err := os.OpenFile(webidFile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		DebugLog("Server", "SPKAC os.OpenFile error: "+err.Error())
+		return 500, err.Error()
+	}
+	defer f.Close()
+
+	// write WebID profile to disk
+	err = g.WriteFile(f, "text/turtle")
+	if err != nil {
+		DebugLog("Server", "SPKAC saving profile error: "+err.Error())
+		return 500, err.Error()
+	}
+
+	cert := base64.StdEncoding.EncodeToString(newSpkac)
+	DebugLog("System", cert)
+
+	// Chrome access direct download of certs; other browsers don't
+	ua := req.Header.Get("User-Agent")
+	if strings.Contains(ua, "Chrome") {
+		w.Header().Set(HCType, "application/x-x509-user-cert; charset=utf-8")
+	}
+	cert = `<iframe width="0" height="0" style="display: none;" src="data:application/x-x509-user-cert;base64,` + cert + `"></iframe>`
+
+	return 200, cert
 }
 
-// AccountStatus implements a basic API to check whether a user account exists on the server
+// accountStatus implements a basic API to check whether a user account exists on the server
 // Response object example:
 // {
 //	method:   "accountStatus",
@@ -51,7 +132,9 @@ func spkac() {
 //             available:   true
 //            }
 // }
-func accountStatus(w http.ResponseWriter, req *httpRequest, root string, resource ldpath) (int, string) {
+func accountStatus(w http.ResponseWriter, req *httpRequest, s *Server) (int, string) {
+	resource, _ := s.pathInfo(req.BaseURI())
+
 	data, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		DebugLog("System", "Read body error: "+err.Error())
@@ -67,6 +150,7 @@ func accountStatus(w http.ResponseWriter, req *httpRequest, root string, resourc
 		DebugLog("System", "Unmarshal error: "+err.Error())
 		return 500, err.Error()
 	}
+	accReq.AccountName = strings.ToLower(accReq.AccountName)
 
 	w.Header().Set(HCType, "application/json")
 	status := "success"
@@ -74,12 +158,12 @@ func accountStatus(w http.ResponseWriter, req *httpRequest, root string, resourc
 	isAvailable := true
 
 	DebugLog("System", "Checking if account <"+accReq.AccountName+"> exists...")
-	stat, err := os.Stat(root + accName + "." + resource.Root)
+	stat, err := os.Stat(s.root + accName + "." + resource.Root)
 	if err != nil {
 		DebugLog("System", "Stat error: "+err.Error())
 	}
 	if stat != nil && stat.IsDir() {
-		DebugLog("System", "Found "+root+accName+"."+resource.Root)
+		DebugLog("System", "Found "+s.root+accName+"."+resource.Root)
 		isAvailable = false
 	}
 
--- system_test.go
+++ system_test.go
@@ -3,11 +3,28 @@ package gold
 import (
 	"bytes"
 	"encoding/json"
+	// "io/ioutil"
 	"net/http"
+	// "net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+// func TestSPKAC(t *testing.T) {
+// 	spkac := `MIICRTCCAS0wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDK/2gvbZk5wajwkX6wwhCrG39NetMycseg8nFgN23MKSLbjU/JspvCrk6jlHNs8d1+FcFyU/AHqYYiY60mSMymDetnho/iqW5sThziyOaVmQ7I7JM6Lqr1tD3376VTvq/1KKrIJrnyCEuxeysflFpS+uTY5X5YV5n8AUPQhjr0aJXnIAI0SryLd0KeSGb+p7uxlmKG7Q8mxl1wel3WXEFr1oVLa61BHfbO8IhrAV8bUBsc0tWX/OSZc611exX1XZ/f3ujxRaL96xraN7AS7/zNI024r4261jPnVTpdFwf2CcnfU7rwCjgcezfBDcIVOUliyUfh1QTRZEYS4LUUVHAHAgMBAAEWBWhlbGxvMA0GCSqGSIb3DQEBBAUAA4IBAQCIBcbE+nw/vpjLvdl7EVnX4TWpKxDej92MOafyaOjNmy/iVhto57Lr+jBhm0A1oHpmGXLarkQPSLcXndZJFm/WSdHZ5pids+fEpe9yyMhgYYkVqqNbnGQmgSrmRZjIbzF6J69SaYXqJ1jQAZ4RrxRsgimfUfGw3C59yytdqkqllg2ojZe158vRlO/X6ysyCevchT9InDAWXE8YM/LBaI6jSlAz1BUFw0phpnAWTpULjMoP45QelY26gfNT1oDD+7PXAiEeo101kba67UcKXr8/7Z05iUONvkE+X1nNLynpvSskz7hha0pjtR+ipDVL9vIQxBFZ1xwrbbOj1fmIKzaE`
+// 	response, _ := http.PostForm(testServer.URL+"/,system/spkac",
+// 		url.Values{"spkac": {spkac},
+// 			"username": {"user"},
+// 			"name":     {"Test User"},
+// 			"email":    {"test@user.org"},
+// 			"img":      {"https://img.org/"},
+// 		})
+// 	body, _ := ioutil.ReadAll(response.Body)
+// 	response.Body.Close()
+// 	println(body)
+// 	//assert.Equal(t, 200, response.StatusCode)
+// }
 
 func TestAccountStatus(t *testing.T) {
 	ar := accountRequest{
