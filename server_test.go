8d3cc06489ac429f6fadbaf12f759d4da107f005
presbrey <presbrey@users.noreply.github.com>
2015-02-11 19:24:35 -0500

    add proxy test

--- server_test.go
+++ server_test.go
@@ -19,6 +19,16 @@ var (
 	handler = NewServer(GetServerRoot(), false)
 )
 
+func TestProxy(t *testing.T) {
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		request, _ := http.NewRequest("GET", ProxyPath+"?uri=http://www.w3.org/ns/auth/acl", nil)
+		response := r.Do(request)
+		assert.Equal(t, 200, response.StatusCode)
+		assert.Contains(t, response.RawResponse.Header.Get("Content-Type"), "application/rdf+xml")
+		assert.Contains(t, response.Body, "<rdf:RDF")
+	})
+}
+
 func TestMKCOL(t *testing.T) {
 	testflight.WithServer(handler, func(r *testflight.Requester) {
 		request, _ := http.NewRequest("MKCOL", "/_test", nil)
