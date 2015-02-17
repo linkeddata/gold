d1fc6450372222dbdd909f7db559ce9673dd1c95
deiu <andrei@fcns.eu>
2015-02-17 01:33:16 -0500

    Added template for accountRecovery email

--- templates.go
+++ templates.go
@@ -39,4 +39,19 @@ jQuery(document).ready(function() {
 </body>
 </html>`,
 	}
+
+	SMTPTemplates = map[string]string{
+		"accountRecovery": `From: {{.From}}
+To: {{.To}}
+Subject: {{.Subject}}
+MIME-version: 1.0
+Content-Type: text/html; charset="UTF-8"
+
+{{.Body}}
+
+Sincerely,
+
+{{.From}}
+`,
+	}
 )
