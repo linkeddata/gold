fa99e3cc795af7d10ea6318626b145e1a3db1aa6
deiu <andrei@fcns.eu>
2016-06-15 13:04:34 -0400

    Added a way to proxy the login app

--- proxy.go
+++ proxy.go
@@ -1,20 +1,17 @@
 package gold
 
 import (
-	"net/http"
-
 	"github.com/elazarl/goproxy"
+	"net/http"
 )
 
-var (
-	proxy = goproxy.NewProxyHttpServer()
-)
-
-func init() {
+func NewProxy() *goproxy.ProxyHttpServer {
+	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnResponse().DoFunc(func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 		r.Header.Set("Access-Control-Allow-Credentials", "true")
 		r.Header.Set("Access-Control-Expose-Headers", "User, Triples, Location, Link, Vary, Last-Modified, Content-Length")
 		r.Header.Set("Access-Control-Max-Age", "60")
 		return r
 	})
+	return proxy
 }
