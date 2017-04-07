7597458c81177d7911224efc0def16f7c0a4a73c
deiu <andrei@fcns.eu>
2017-04-07 13:27:43 -0400

    fixes HTTP/2 compatibility

--- proxy.go
+++ proxy.go
@@ -12,9 +12,14 @@ var (
 
 func init() {
 	proxy.OnResponse().DoFunc(func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+		if r == nil {
+			return r
+		}
 		r.Header.Set("Access-Control-Allow-Credentials", "true")
 		r.Header.Set("Access-Control-Expose-Headers", "User, Triples, Location, Link, Vary, Last-Modified, Content-Length")
 		r.Header.Set("Access-Control-Max-Age", "60")
+		// Drop connection to allow for HTTP/2 <-> HTTP/1.1 compatibility
+		r.Header.Del("Connection")
 		return r
 	})
 }
